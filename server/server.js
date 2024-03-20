0561282e6b0bca5bf8cc1be26d514ad0e3c40de2
kazalnsl <kazal@nextsolutionlab.com>
2024-03-20 17:18:48 +0600

    first commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = require("./app");
+
+/* Loading the environment variables from the .env file. */
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/productivity-app";
+
+/* Connecting to the database and then starting the server. */
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(PORT, console.log("Server stated on port 5000"));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  app.use(cors());
